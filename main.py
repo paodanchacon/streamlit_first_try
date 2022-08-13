6b963421ac4a40ed395d968c11f8d92b1a80e0d8
Daniela Chambilla <63526399+paodanchacon@users.noreply.github.com>
2022-08-13 10:33:23 -0400

    Erased animation

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import streamlit as st
 import requests 
-from streamlit_lottie import st_lottie
+#from streamlit_lottie import st_lottie
 
 st.set_page_config(page_title="Python Tricks", page_icon=":snake:", layout="wide")
 
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+#def load_lottieurl(url):
+#    r = requests.get(url)
+#    if r.status_code != 200:
+#        return None
+#    return r.json()
+
 # ------ ANIMATIONS ------
-lottie_coding = load_lottieurl("https://assets4.lottiefiles.com/packages/lf20_2znxgjyt.json")
+#lottie_coding = load_lottieurl("https://assets4.lottiefiles.com/packages/lf20_2znxgjyt.json")
 
 # ------ HEADER SECTION ------
-left_column, right_column = st.columns(2)
-with right_column:
-    st_lottie(lottie_coding, height=120, key="coding")
+#left_column, right_column = st.columns(2)
+#with right_column:
+#    st_lottie(lottie_coding, height=120, key="coding")
 
 with st.container():
     
@@ -27,6 +28,7 @@ with st.container():
 from PIL import Image
 image = Image.open("img\Mr_Pineapple.jpg")
 st.image(image, caption="""Photo by [Miesha Maiden](https://www.pexels.com/@miphotography?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels) from [Pexels](https://www.pexels.com/photo/pineapple-with-brown-sunglasses-459601/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels)""")
+st.caption("Photo by [Miesha Maiden](https://www.pexels.com/@miphotography?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels) from [Pexels](https://www.pexels.com/photo/pineapple-with-brown-sunglasses-459601/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels)")
 #st.image(image, caption=(st.write("Photo by"), st.write("[Miesha Maiden](https://www.pexels.com/@miphotography?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels)")))
 #st.write("[Learn More >](https://pranjalai.medium.com/)")
 st.write("The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities.")
