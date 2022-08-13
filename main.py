f8103e4f9fd6fc876175246b03172e7dc3ab307a
Daniela Chambilla <63526399+paodanchacon@users.noreply.github.com>
2022-08-13 10:55:37 -0400

    Added aimation

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import streamlit as st
 import requests 
-#from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie
 
 st.set_page_config(page_title="Python Tricks", page_icon=":snake:", layout="wide")
 
-#def load_lottieurl(url):
-#    r = requests.get(url)
-#    if r.status_code != 200:
-#        return None
-#    return r.json()
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
 # ------ ANIMATIONS ------
-#lottie_coding = load_lottieurl("https://assets4.lottiefiles.com/packages/lf20_2znxgjyt.json")
+lottie_coding = load_lottieurl("https://assets4.lottiefiles.com/packages/lf20_2znxgjyt.json")
 
 # ------ HEADER SECTION ------
-#left_column, right_column = st.columns(2)
-#with right_column:
-#    st_lottie(lottie_coding, height=120, key="coding")
+left_column, right_column = st.columns(2)
+with right_column:
+    st_lottie(lottie_coding, height=120, key="coding")
 
 with st.container():
     
