5d3b932400b0f11d373563434326046918e52c5c
Daniela Chambilla <63526399+paodanchacon@users.noreply.github.com>
2022-08-13 11:26:15 -0400

    Erased the animation library

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import requests
+#import requests
 import streamlit as st
-from streamlit_lottie import st_lottie_spinner
+#from streamlit_lottie import st_lottie_spinner
 
 st.set_page_config(page_title="Python Tricks", page_icon=":snake:", layout="wide")
 
